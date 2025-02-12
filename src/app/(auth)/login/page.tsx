d07eca10bc77ddd42114e73a6ab7263e4e43d0f8
yadhukrishnan <yadhukrishnan.p@jiitak.com>
2025-02-12 17:07:33 +0530

    code cleanup and bug fix

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import styles from './login.module.css';
-import LoginForm from '@/modules/login/components/login-form/login-form';
 import Login from '@/modules/login/views/login';
 
 const LoginPage = () => {
