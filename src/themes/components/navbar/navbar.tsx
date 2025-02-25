4c1b586b5b76c37dc4b4f5055227635006ffc077
yadhukrishnan <yadhukrishnan.p@jiitak.com>
2025-02-25 17:04:32 +0530

    test

--- src/themes/components/navbar/navbar.tsx
+++ src/themes/components/navbar/navbar.tsx
@@ -7,16 +7,17 @@ interface NavbarProps{
 
 const Navbar:React.FC<NavbarProps> = ({ additionalContents = null }) => {  
     return (
-      <div className={styles.navbar}>
-        {/* Logo Section */}
-        <div className={styles.logo}>
-          <img src="/icons/logo.svg" alt="Logo" />
-          <h2 className={styles.logoHeader}>Service Name</h2>
-        </div>
+      // <div className={styles.navbar}>
+      //   {/* Logo Section */}
+      //   <div className={styles.logo}>
+      //     <img src="/icons/logo.svg" alt="Logo" />
+      //     <h2 className={styles.logoHeader}>Service Name</h2>
+      //   </div>
   
-        {/* Optional additional contents */}
-        {additionalContents && <div className={styles.additional}>{additionalContents}</div>}
-      </div>
+      //   {/* Optional additional contents */}
+      //   {additionalContents && <div className={styles.additional}>{additionalContents}</div>}
+      // </div>
+      <></>
     );
   };
   
